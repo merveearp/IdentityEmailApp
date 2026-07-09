401acdcb11faa878888036947303ffdc5d0f2d00
Merve Arpacıoğlu Türk <merveearp@icloud.com>
2026-07-09 16:13:44 +0300

    add to profile controller

---
+++ IdentityEmailApp/Controllers/ProfileController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityEmailApp.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult EditProfile()
+        {
+            return View();
+        }
+
+    }
+}
