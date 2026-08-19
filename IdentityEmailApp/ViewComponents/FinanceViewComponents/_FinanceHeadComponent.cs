5842239e0fa1dc7a8f1c21ad1420aebbcb0e54ba
Merve Arpacıoğlu Türk <merveearp@icloud.com>
2026-08-19 23:05:13 +0300

    add to financeController

---
+++ IdentityEmailApp/ViewComponents/FinanceViewComponents/_FinanceHeadComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityEmailApp.ViewComponents.FinanceViewComponents
+{
+    public class _FinanceHeadComponent:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
